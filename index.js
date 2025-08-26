1e8b08361b984a79b5983fe0397de5720b56aa61
Edgard Chavez <edgard@MacBook-Air-de-Edgard.local>
2025-08-26 17:20:18 -0400

    Ajustes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors({
   origin: true,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 
